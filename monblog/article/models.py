8b517843ee7148d8491576e41dfc4fa11312a568
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-04-17 14:05:36 +0200

    models

--- monblog/article/models.py
+++ monblog/article/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    titre = models.CharField(max_length=100)
+    auteur = models.ForeignKey(User, on_delete=models.RESTRICT)
+    contenu = models.TextField()
+
+    def __str__(self):
+        return f'{self.titre} par {self.auteur}'
